25cc9652ce8eb253a0cc0c3582ed25e0c7f89d7a
Santiago Dimaren <sdimaren1@gmail.com>
2021-02-09 16:36:24 -0500

    rendering a list of posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,12 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>
-    {children}
-  </BlogContext.Provider>
+  const blogPosts = [
+    { title: 'Blog Post #1'},
+    { title: 'Blog Post #2'}
+  ];
+
+  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
 };
+
+export default BlogContext;
